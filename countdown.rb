81346cb5ad9cae77b258c86092e784728522fefc
conner fortner <fortner.conner@yahoo.com>
2020-09-15 19:51:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,6 @@
 def countdown(x)
   while x >= 0
-  puts "#{x} SECONDS!"
+  puts "#{x} SECOND(S)!"
   x -=1
   end
  return "HAPPY NEW YEAR!"
@@ -8,7 +8,7 @@ end
 
 def countdown_with_sleep(x)
   while x >= 0
-    puts "#{x} SECONDS!" 
+    puts "#{x} SECOND(S)!" 
     sleep(1)
     x -=1
   end
